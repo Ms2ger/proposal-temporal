c1ecc74840ca7e2617ea36e6a2ac38cfa01889ad
Ms2ger <Ms2ger@gmail.com>
2019-11-15 16:18:32 +0100

    [wip] Initial setup for a test262-based test suite.

---
+++ polyfill/ci_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export NODE_PATH=$PWD/node_modules
+npm install test262-harness esm
+git clone --depth 1 https://github.com/tc39/test262.git
+
+test262-harness --test262Dir ./test262 --prelude prelude.js --hostArgs="-r esm" test/test262.js | tee exec.out
+if grep -q '^[1-9][0-9]* failed$' exec.out; then
+  rm exec.out
+  exit 1
+fi
+
+rm exec.out
